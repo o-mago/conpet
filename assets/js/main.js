9741a4c4aeab884dd627560de6bb4613e39020de
Alexandre Cabral <alexandre.cabral@engenharia.ufjf.br>
2018-05-09 00:47:00 -0300

    Site SHOW

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,24 @@
  * Change Navbar color while scrolling
 */
 
-$(window).scroll(function () {
-  handleTopNavAnimation();
-});
+// $(window).scroll(function () {
+//   handleTopNavAnimation();
+// });
 
-$(window).load(function () {
-  handleTopNavAnimation();
-});
+// $(window).load(function () {
+//   handleTopNavAnimation();
+// });
 
-function handleTopNavAnimation() {
-  var top = $(window).scrollTop();
+// function handleTopNavAnimation() {
+//   var top = $(window).scrollTop();
 
-  if (top > 10) {
-    $('#site-nav').addClass('navbar-solid');
-  }
-  else {
-    $('#site-nav').removeClass('navbar-solid');
-  }
-}
+//   if (top > 10) {
+//     $('#site-nav').addClass('navbar-solid');
+//   }
+//   else {
+//     $('#site-nav').removeClass('navbar-solid');
+//   }
+// }
 
 // function mudaImagem() {
 
